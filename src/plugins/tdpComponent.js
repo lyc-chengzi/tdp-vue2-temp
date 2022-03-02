57bd18141864a7b187c8c36d007d34c3caa23434
liuyc14 <liuyc14@lenovo.com>
2022-03-02 15:18:29 +0800

    应用$axios,$qs

--- src/plugins/tdpComponent.js
+++ src/plugins/tdpComponent.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import SearchForm from '@smartplatform/ui-components/src/index';
 import VeCharts from '@smartplatform/ui-components/src/ve-chart/index';
+import axios from 'axios';
+import qs from 'qs';
 // import Custom from '@smartplatform/ui-components/src/custom/index';
 
+Vue.prototype.$qs = qs;
+Vue.prototype.$axios = axios;
 Vue.use(SearchForm);
 Vue.use(VeCharts);
 // Vue.use(Custom);
